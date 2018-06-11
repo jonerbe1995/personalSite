efcdef67dab0f578a4eccad872c3dcbf474b4c93
Jonathan Erbe <jonerbe1995@gmail.com>
2018-06-11 13:48:09 -0500

    Reorg

--- core.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-
-var server = http.createServer(function(req, res){
-	res.writeHead(200, {"Content-Type": "text/html"});
-});
-
-server.listen(3000);
-
-console.log("Server running on port 3000");
--- public/js/core.js
+++ public/js/core.js
@@ -1,6 +1,12 @@
+var http = require("http");
+var server = http.createServer(function(req, res){
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("Hello world");
+});
 var WebSocketServer = require("ws").Server;
 var wss = new WebSocketServer({port : 3000});
 
+server.listen(3000);
 wss.on("connection", function(ws){
 	ws.send("Welcome!");
 });
